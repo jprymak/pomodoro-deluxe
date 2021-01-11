0792d270812bda422f09dff2f268eca1f3d22636
Jakub Prymak <jprymak@gmail.com>
2021-01-11 22:41:50 +0100

    style: set App component display to flex

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TaskCreator from "./components/TaskCreator"
 
 function App() {
   return (
-    <div className="App h-screen">
+    <div className="App h-screen flex justify-center items-center">
       <TaskCreator/>
     </div>
   );
