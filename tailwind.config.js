cafe7bcb810f935a5bee9ce20534037976bd4128
Jakub Prymak <jprymak@gmail.com>
2021-01-11 20:36:06 +0100

    build: change outline black class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      outline: {
+        black: '1px solid black',
+      }
+    },
   },
   variants: {
     extend: {},
