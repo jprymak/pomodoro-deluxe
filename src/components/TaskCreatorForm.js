72fe34d0c6cf16b4b65164d3e0b4942e3bd16982
Jakub Prymak <jprymak@gmail.com>
2021-01-11 22:43:11 +0100

    style: set TaskCreatorForm sizing and margins

--- src/components/TaskCreatorForm.js
+++ src/components/TaskCreatorForm.js
@@ -1,8 +1,8 @@
 function TaskCreatorForm() {
   return (
-    <div className="container mx-auto h-screen flex items-center justify-center">
-      <form className="flex flex-col items-start justify-start p-2 border-2 border-solid border-black rounded-md">
-        <label htmlFor="" className="my-1 w-full flex justify-between">
+    <div className="mx-auto flex items-center justify-center mb-6 ">
+      <form className="w-auto min-w-min flex flex-col items-start justify-start p-2 border-2 border-solid border-black rounded-md">
+        <label htmlFor="" className="my-1 w-full flex justify-between w-96">
           What are you doing?
           <input className="outline-black ml-3 px-1" type="text" />
         </label>
@@ -18,7 +18,9 @@ function TaskCreatorForm() {
           Number of sessions
           <input className="outline-black ml-3 px-1" type="number" />
         </label>
-        <button className="p-1 self-center border-solid border-2 border-black rounded-md mt-4">Accept</button>
+        <button className="p-1 self-center border-solid border-2 border-black rounded-md mt-4">
+          Accept
+        </button>
       </form>
     </div>
   );
