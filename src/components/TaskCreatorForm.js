ed4cb57522446c2546a49e278725d6cb8c5d8430
Jakub Prymak <jprymak@gmail.com>
2021-01-13 20:38:15 +0100

    feat: make TaskCreatorForm a controllable component

--- src/components/TaskCreatorForm.js
+++ src/components/TaskCreatorForm.js
@@ -1,22 +1,55 @@
-function TaskCreatorForm() {
+function TaskCreatorForm({
+  onSubmit,
+  task,
+  sessionLengthInMinutes,
+  breakLengthInMinutes,
+  numberOfSessions,
+  onTaskNameChange,
+  onSessionLengthInMinutesChange,
+  onBreakLengthInMinutesChange,
+  onNumberOfSessionsChange,
+}) {
   return (
     <div className="mx-auto flex items-center justify-center mb-6 ">
-      <form className="w-auto min-w-min flex flex-col items-start justify-start p-2 border-2 border-solid border-black rounded-md">
+      <form
+        onSubmit={onSubmit}
+        className="w-auto min-w-min flex flex-col items-start justify-start p-2 border-2 border-solid border-black rounded-md"
+      >
         <label htmlFor="" className="my-1 w-full flex justify-between w-96">
           What are you doing?
-          <input className="outline-black ml-3 px-1" type="text" />
+          <input
+            value={task}
+            onChange={onTaskNameChange}
+            className="outline-black ml-3 px-1"
+            type="text"
+          />
         </label>
         <label htmlFor="" className="my-1 w-full flex justify-between">
           Set session length in minutes
-          <input className="outline-black ml-3 px-1" type="number" />
+          <input
+            value={sessionLengthInMinutes}
+            onChange={onSessionLengthInMinutesChange}
+            className="outline-black ml-3 px-1"
+            type="number"
+          />
         </label>
         <label htmlFor="" className="my-1 w-full flex justify-between">
           Set break length in minutes
-          <input className="outline-black ml-3 px-1" type="number" />
+          <input
+            value={breakLengthInMinutes}
+            onChange={onBreakLengthInMinutesChange}
+            className="outline-black ml-3 px-1"
+            type="number"
+          />
         </label>
         <label htmlFor="" className="my-1 w-full flex justify-between">
           Number of sessions
-          <input className="outline-black ml-3 px-1" type="number" />
+          <input
+            value={numberOfSessions}
+            onChange={onNumberOfSessionsChange}
+            className="outline-black ml-3 px-1"
+            type="number"
+          />
         </label>
         <button className="p-1 self-center border-solid border-2 border-black rounded-md mt-4">
           Accept
