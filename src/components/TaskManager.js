ae1f2bb8689f89d12d28cabfebc661adfc051137
Jakub Prymak <jprymak@gmail.com>
2021-01-16 17:44:18 +0100

    feat: create TaskManager component

---
+++ src/components/TaskManager.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class TaskManager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+    };
+  }
+
+
+
+  render() {
+    return <div className="w-full border-black"></div>;
+  }
+}
+
+export default TaskManager;
