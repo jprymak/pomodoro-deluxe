9b1428d0308b1cb79c2660b6cbebefd0e7977aeb
Jakub Prymak <jprymak@gmail.com>
2021-01-17 14:09:11 +0100

    feat: create Task component and move CurrentSession content there

---
+++ src/components/Task.js
@@ -0,0 +1,46 @@
+function Task({ currentSession }) {
+  const {
+    task,
+    sessionBlockWidth,
+    sessionLengthInMinutes,
+    breakBlockWidth,
+    breakLengthInMinutes,
+    previewBlocks,
+  } = currentSession;
+  return (
+    <div className="h-40 w-full">
+      <h2 className="mx-auto w-full text-center mb-2">{task}</h2>
+      <div className="w-full border-2 border-solid border-black rounded-md h-3/6 p-1 flex">
+        {previewBlocks.map((block) => {
+          return block == "session" ? (
+            <div
+              style={{ width: `${sessionBlockWidth}%` }}
+              className="flex flex-col"
+            >
+              <p className="w-full text-center">{sessionLengthInMinutes}</p>
+              <div className="h-full bg-red-400"></div>
+            </div>
+          ) : (
+            <div
+              style={{ width: `${breakBlockWidth}%` }}
+              className="flex flex-col"
+            >
+              <p className="w-full text-center">{breakLengthInMinutes}</p>
+              <div className="h-full bg-black"></div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-around">
+        <button className="p-1 self-center border-solid border-2 border-black rounded-md mt-4 w-20">
+          Start
+        </button>
+        <button className="p-1 self-center border-solid border-2 border-black rounded-md mt-4 w-20">
+          Pause
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Task;
