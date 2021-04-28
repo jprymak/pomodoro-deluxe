21b46d92776ae85bc493aadd2ffd85e2cf5bbea2
Jakub Prymak <jprymak@gmail.com>
2021-04-28 23:11:00 +0200

    style: center Timer component and make it bigger

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,10 +3,8 @@
 function Timer(props){
     const {hours,minutes,seconds} = props;
     return(
-        <div>
-            {hours <10 ? "0"+ hours : hours}:
-            {minutes<10 ? "0"+ minutes : minutes}:
-            {seconds<10 ? "0"+ seconds : seconds}</div>
+        <div className="timer">
+            {hours <10 ? "0"+ hours : hours} : {minutes<10 ? "0"+ minutes : minutes} : {seconds<10 ? "0"+ seconds : seconds}</div>
     )
 }
 
