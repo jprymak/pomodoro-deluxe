5366a9ae0c463d0ff4cceedca07dd014f51cec7a
Jakub Prymak <jprymak@gmail.com>
2021-05-03 15:45:07 +0200

    feat: change navigation link style when active (add after svg and change color)

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { ReactComponent as Tomato } from '../../assets/tomato-svgrepo-com.svg';
+import { useLocation } from "react-router-dom";
 
 function NavBar() {
+
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+
   return (
     <nav className="navbar">
       <Tomato className="navbar__icon"/>
       <ul className="navbar__list">
-        <li className="navbar__link">
-          <Link exact to="/">Current Session</Link>
+        <li className={`navbar__link ${splitLocation[1] === "" ? "navbar__link--active" : ""}`}>
+          <NavLink exact className=""  to="/">Current Session</NavLink>
         </li>
-        <li className="navbar__link">
-          <Link to="/task-creator">Task Creator</Link>
+        <li className={`navbar__link ${splitLocation[1] === "task-creator" ? "navbar__link--active" : ""}`}>
+          <NavLink to="/task-creator">Task Creator</NavLink>
         </li>
-        <li className="navbar__link">
-          <Link to="/task-manager">Task Manager</Link>
+        <li className={`navbar__link ${splitLocation[1] === "task-manager" ? "navbar__link--active" : ""}`}>
+          <NavLink to="/task-manager">Task Manager</NavLink>
         </li>
-        <li className="navbar__link">
-          <Link to="/history">History</Link>
+        <li className={`navbar__link ${splitLocation[1] === "history" ? "navbar__link--active" : ""}`}>
+          <NavLink to="/history">History</NavLink>
         </li>
       </ul>
     </nav>
