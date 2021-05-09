6b022625a090e17aba49f88f644ba701fa73dc3f
Jakub Prymak <jprymak@gmail.com>
2021-05-09 20:19:30 +0200

    fix: change BrowserRouter to HashRouter get rid of gh-pages 404 error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import './App.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App path="/" component={App}/>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
