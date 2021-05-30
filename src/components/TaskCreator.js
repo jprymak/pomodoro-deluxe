e70fc1f7088f73253e268b690175194966eb65e5
Jakub Prymak <jprymak@gmail.com>
2021-05-30 11:48:26 +0200

    refactor: convert TaskCreator to functional component with hooks

--- src/components/TaskCreator.js
+++ src/components/TaskCreator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import TaskCreatorForm from "./TaskCreatorForm";
 import TaskPreview from "./TaskPreview";
@@ -7,47 +7,69 @@ import { v4 as uuidv4 } from "uuid";
 
 import defaultTask from "../lib/defaultTask"
 
-class TaskCreator extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = defaultTask;
-  }
+function TaskCreator({onTaskCreation}){
   
-  handleSubmit = (event)=>{
+  const [taskData, setTaskData] = useState(defaultTask)
+  const [hasError, setHasError] = useState(false)
+  
+useEffect(()=>{
+  function createPreviewBlocks(){
+    setTaskData(
+      (prev) => {
+        const previewBlocks = [];
+        for (let i = 1; i <= taskData.numberOfSessions; i++) {
+          if (i < taskData.numberOfSessions) {
+            previewBlocks.push("session", "break");
+          } else {
+            previewBlocks.push("session");
+          }
+        }
+        return {...prev, previewBlocks };
+      },
+    );
+  };
+  createPreviewBlocks();
+},[taskData.numberOfSessions])
+
+  const handleSubmit = (event)=>{
     event.preventDefault()
-    if(this.validate()){
-      this.props.onTaskCreation({...this.state, id: uuidv4(), alarmTimeStamps: this.createAlarmTimeStamps()})
-      this.setState({...defaultTask})
+    if(validate()){
+      onTaskCreation({id: uuidv4(), alarmTimeStamps: createAlarmTimeStamps(),...taskData })
+      setTaskData({...defaultTask})
     } 
-    else this.setState({hasError:true})
+    else setHasError(true)
   }
 
-  handleTaskNameChange = (event) => {
-    this.checkIfValueIsAboveLimit("sessionName", event.target.value) && this.setState({ task: event.target.value }, () => {
-      //  this.createPreviewBlocks();
+  const handleTaskNameChange = (event) => {
+    checkIfValueIsAboveLimit("sessionName", event.target.value) && setTaskData(prev=>{
+      const task = event.target.value
+      return {...prev, task}
     });
     
   };
 
-  handleSessionLengthInMinutesChange = (event) => {
-    this.checkIfValueIsAboveLimit("sessionLength", event.target.value) && this.setState({ sessionLengthInMinutes: event.target.value }, () => {
-      //  this.createPreviewBlocks();
+  const handleSessionLengthInMinutesChange = (event) => {
+    checkIfValueIsAboveLimit("sessionLength", event.target.value) && setTaskData(prev=>{
+      const sessionLengthInMinutes = event.target.value;
+      return {...prev, sessionLengthInMinutes}
     });
   };
 
-  handleBreakLengthInMinutesChange = (event) => {
-    this.checkIfValueIsAboveLimit("breakLength", event.target.value) && this.setState({ breakLengthInMinutes: event.target.value }, () => {
-      // this.createPreviewBlocks();
+  const handleBreakLengthInMinutesChange = (event) => {
+    checkIfValueIsAboveLimit("breakLength", event.target.value) && setTaskData(prev=>{
+      const breakLengthInMinutes = event.target.value;
+      return {...prev, breakLengthInMinutes}
     });
   };
 
-  handleNumberOfSessionsChange = (event) => {
-    this.checkIfValueIsAboveLimit("numberOfSessions", event.target.value) && this.setState({ numberOfSessions: event.target.value }, () => {
-      this.createPreviewBlocks();
-    });
+  const handleNumberOfSessionsChange = (event) => {
+    checkIfValueIsAboveLimit("numberOfSessions", event.target.value) && setTaskData(prev=>{
+      const numberOfSessions = +event.target.value;
+      return {...prev, numberOfSessions}
+    })
   };
 
-  checkIfValueIsAboveLimit = (inputName, value) => {
+  const checkIfValueIsAboveLimit = (inputName, value) => {
     const inputsUpperLimits = {
       "sessionName": value.length <= 40,
       "sessionLength": value <= 60,
@@ -57,79 +79,58 @@ class TaskCreator extends React.Component {
     return inputsUpperLimits[inputName]
   }
 
-  validate = () =>{
+  const validate = () =>{
     const conditions = [
-      (this.state.task.length > 0 && this.state.task.length <= 40),
-      this.state.sessionLengthInMinutes >= 10 && this.state.sessionLengthInMinutes <= 60,
-      this.state.breakLengthInMinutes >= 5 && this.state.breakLengthInMinutes <= 60,
-      this.state.numberOfSessions > 0 && this.state.numberOfSessions <= 10,
+      (taskData.task.length > 0 && taskData.task.length <= 40),
+      taskData.sessionLengthInMinutes >= 10 && taskData.sessionLengthInMinutes <= 60,
+      taskData.breakLengthInMinutes >= 5 && taskData.breakLengthInMinutes <= 60,
+      taskData.numberOfSessions > 0 && taskData.numberOfSessions <= 10,
     ]
 
     return conditions.every(condition=>condition===true);
   }
 
-  createAlarmTimeStamps = () =>{
+  const createAlarmTimeStamps = () =>{
 
     const alarmTimeStamps = [];
     let lastTimeStamp = 0;
-    for (let i = 1; i <= this.state.numberOfSessions; i++) {
+    for (let i = 1; i <= taskData.numberOfSessions; i++) {
       
-      if (i < this.state.numberOfSessions) {
-        lastTimeStamp = lastTimeStamp + this.state.sessionLengthInMinutes*60
+      if (i < taskData.numberOfSessions) {
+        lastTimeStamp = lastTimeStamp + taskData.sessionLengthInMinutes*60
         alarmTimeStamps.push({timeStamp: lastTimeStamp, status: "sessionEnded"});
-        lastTimeStamp = lastTimeStamp + this.state.breakLengthInMinutes*60
+        lastTimeStamp = lastTimeStamp + taskData.breakLengthInMinutes*60
         alarmTimeStamps.push({timeStamp: lastTimeStamp, status: "breakEnded"});
       } else{
-        lastTimeStamp = lastTimeStamp + this.state.sessionLengthInMinutes*60
+        lastTimeStamp = lastTimeStamp + taskData.sessionLengthInMinutes*60
         alarmTimeStamps.push({timeStamp: lastTimeStamp, status: "sessionEnded"});
       }
     }
     return alarmTimeStamps
   }
 
-  setFirstTimeStamp = () =>{
-    
-  }
+  
 
-  createPreviewBlocks = () => {
-    this.setState(
-      (prevState) => {
-        const previewBlocks = [];
-        for (let i = 1; i <= this.state.numberOfSessions; i++) {
-          if (i < this.state.numberOfSessions) {
-            previewBlocks.push("session", "break");
-          } else {
-            previewBlocks.push("session");
-          }
-        }
-        return { previewBlocks };
-      },
-    );
-  };
-
-  render() {
-    
     return (
       <div className="task-creator">
         <h1 className="task-creator__heading">TASK CREATOR</h1>
         <TaskCreatorForm
-          hasError ={this.state.hasError}
-          task={this.state.task}
-          sessionLengthInMinutes={this.state.sessionLengthInMinutes}
-          breakLengthInMinutes={this.state.breakLengthInMinutes}
-          numberOfSessions={this.state.numberOfSessions}
-          onTaskNameChange={this.handleTaskNameChange}
+          hasError ={hasError}
+          task={taskData.task}
+          sessionLengthInMinutes={taskData.sessionLengthInMinutes}
+          breakLengthInMinutes={taskData.breakLengthInMinutes}
+          numberOfSessions={taskData.numberOfSessions}
+          onTaskNameChange={handleTaskNameChange}
           onSessionLengthInMinutesChange={
-            this.handleSessionLengthInMinutesChange
+            handleSessionLengthInMinutesChange
           }
-          onBreakLengthInMinutesChange={this.handleBreakLengthInMinutesChange}
-          onNumberOfSessionsChange={this.handleNumberOfSessionsChange}
-          onSubmit={this.handleSubmit}
+          onBreakLengthInMinutesChange={handleBreakLengthInMinutesChange}
+          onNumberOfSessionsChange={handleNumberOfSessionsChange}
+          onSubmit={handleSubmit}
         />
-        <TaskPreview formValue={this.state} />
+        <TaskPreview formValue={taskData} />
       </div>
     );
-  }
 }
 
 export default TaskCreator;
