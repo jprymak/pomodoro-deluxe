2e70829929ba86f5089340d76fe1bb80814e5992
Jakub Prymak <jprymak@gmail.com>
2021-05-30 12:55:35 +0200

    refactor: move initial tasks and session to seperate module

--- src/App.js
+++ src/App.js
@@ -6,109 +6,16 @@ import TaskCreator from "./components/TaskCreator";
 import TaskManager from "./components/TaskManager";
 import NavBar from "./components/navbar/NavBar";
 import CurrentSession from "./components/CurrentSession";
-import History from "./components/History"
+import History from "./components/History";
 
-import { v4 as uuidv4 } from "uuid";
+import initialTasks from "./lib/initialTasks";
+import initialSession from "./lib/initialSession";
 
 class App extends React.Component {
   state = {
-    tasks: [
-      {
-        id: uuidv4(),
-        task: "Learning React",
-        sessionLengthInMinutes: 25,
-        breakLengthInMinutes: 5,
-        numberOfSessions: 4,
-        previewBlocks: [
-          "session",
-          "break",
-          "session",
-          "break",
-          "session",
-          "break",
-          "session",
-        ],
-        elapsedTimeInSeconds: 0,
-        isPaused: false,
-        isRunning: false,
-        alarmTimeStamps: [{timeStamp: 1500, status: "sessionEnded"},
-       {timeStamp: 1800, status: "breakEnded"},
-       {timeStamp: 3300, status: "sessionEnded"},
-       {timeStamp: 3600, status: "breakEnded"},
-       {timeStamp: 5100, status: "sessionEnded"},
-       {timeStamp: 5400, status: "breakEnded"},
-       {timeStamp: 6900, status: "sessionEnded"}
-    
-    ],
-      nextTimeStampIndex: 0,
-      nextTimeStamp: "",
-      isPlaying: false
-      },
-      {
-        id: uuidv4(),
-        task: "Learning Agular",
-        sessionLengthInMinutes: 25,
-        breakLengthInMinutes: 5,
-        numberOfSessions: 4,
-        previewBlocks: [
-          "session",
-          "break",
-          "session",
-          "break",
-          "session",
-          "break",
-          "session",
-        ],
-        elapsedTimeInSeconds: 0,
-        isPaused: false,
-        isRunning: false,
-        alarmTimeStamps: [{timeStamp: 1500, status: "sessionEnded"},
-       {timeStamp: 1800, status: "breakEnded"},
-       {timeStamp: 3300, status: "sessionEnded"},
-       {timeStamp: 3600, status: "breakEnded"},
-       {timeStamp: 5100, status: "sessionEnded"},
-       {timeStamp: 5400, status: "breakEnded"},
-       {timeStamp: 6900, status: "sessionEnded"}
-    
-    ],
-      nextTimeStampIndex: 0,
-      nextTimeStamp: "",
-      isPlaying: false
-      },
-    ],
-    currentSession: {
-      id: uuidv4(),
-      task: "Learning Vue",
-      sessionLengthInMinutes: 25,
-      breakLengthInMinutes: 5,
-      numberOfSessions: 4,
-      previewBlocks: [
-        "session",
-        "break",
-        "session",
-        "break",
-        "session",
-        "break",
-        "session",
-      ],
-      elapsedTimeInSeconds: 0,
-      isPaused: false,
-      isRunning: false,
-      alarmTimeStamps: [{timeStamp: 1500, status: "sessionEnded"},
-       {timeStamp: 1800, status: "breakEnded"},
-       {timeStamp: 3300, status: "sessionEnded"},
-       {timeStamp: 3600, status: "breakEnded"},
-       {timeStamp: 5100, status: "sessionEnded"},
-       {timeStamp: 5400, status: "breakEnded"},
-       {timeStamp: 6900, status: "sessionEnded"}
-    
-    ],
-      nextTimeStampIndex: 0,
-      nextTimeStamp: "",
-      isPlaying: false
-    },
-    currentSessionState: {},
-  };
+    tasks: [...initialTasks],
+    currentSession: {...initialSession}
+  }
 
   handleTaskCreation = (newTask) => {
     this.setState((prevState) => {
@@ -120,11 +27,9 @@ class App extends React.Component {
   handleTaskPick = (task, indexToRemove) => {
     const previousTask = this.state.currentSession;
     this.setState({ currentSession: task }, () => {
-      console.log(this.state.currentSession);
     });
     this.removeTask(task, indexToRemove)
     this.setState((prevState) => {
-      console.log(previousTask);
       const tasks = [...prevState.tasks, previousTask];
       return { tasks };
     });
