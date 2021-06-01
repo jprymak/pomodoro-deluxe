40dc1c39ad95474d864205f3fc37551e3bdd06f4
Jakub Prymak <jprymak@gmail.com>
2021-06-01 20:10:19 +0200

    refactor: convert state control to useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
-import {Switch, Route} from "react-router-dom";
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import React, { useState, useReducer } from "react";
+import { Switch, Route } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import TaskCreator from "./components/TaskCreator";
 import TaskManager from "./components/TaskManager";
@@ -11,85 +11,113 @@ import History from "./components/History";
 import initialTasks from "./lib/initialTasks";
 import initialSession from "./lib/initialSession";
 
-function App(){
-  
-  const [tasks, setTasks] = useState(initialTasks);
-  const [currentSession, setCurrentSession] = useState(initialSession);
-  
+const initialState = {
+  tasks: initialTasks,
+  currentSession: initialSession
+}
+
+function stateReducer(prevState, action) {
+
+  if (action.type === "CREATE") {
+    const { newTask } = action;
+    const newTasks = [...prevState.tasks, newTask];
+    return {
+      ...prevState,
+      tasks: newTasks
+    };
+  }
+
+  if (action.type === "DELETE") {
+    const { indexToRemove } = action;
+    const newTasks = prevState.tasks.filter(
+      (task, index) => index !== indexToRemove
+    );
+    return {
+      ...prevState,
+      tasks: newTasks
+    };
+  }
+
+  if (action.type === "PICK") {
+    const { indexToRemove, task } = action;
+    const prevTask = prevState.currentSession
+    const newTasks = prevState.tasks.filter(
+      (task, index) => index !== indexToRemove
+    );
+    return {
+      currentSession: task,
+      tasks: [...newTasks, prevTask]
+    };
+
+  }
+  if (action.type === "SAVE") {
+    const { sentState } = action;
+    const updatedCurrentSession = { ...prevState.currentSession, ...sentState }
+    return {
+      ...prevState,
+      currentSession: updatedCurrentSession,
+    };
+
+  }
+  else return prevState
+}
+
+function App() {
+  const [state, stateDispatch] = useReducer(stateReducer, initialState);
+
   const handleTaskCreation = (newTask) => {
-    setTasks((prev) => {
-      const tasks = [...prev, newTask];
-      return tasks;
-    });
+    stateDispatch({ type: 'CREATE', newTask })
   };
 
   const handleTaskPick = (task, indexToRemove) => {
-    
-    const previousTask = currentSession;
-    setCurrentSession(task);
-    removeTask(task, indexToRemove)
-    
-    setTasks((prev) => {
-      const tasks = [...prev, previousTask];
-      return tasks;
-    });
+    stateDispatch({ type: 'PICK', task, indexToRemove })
   };
 
-  const handleTaskDelete = (task, indexToRemove) =>{
-    removeTask(task, indexToRemove)
-  }
-
-  const removeTask = (task, indexToRemove)=>{
-    setTasks((prev) => {
-      const tasks = prev.filter(
-        (task, index) => index !== indexToRemove
-      );
-      return tasks;
-    });
+  const handleTaskDelete = (task, indexToRemove) => {
+    stateDispatch({ type: 'DELETE', task, indexToRemove })
   }
 
   const handleSaveState = (sentState) => {
-    setCurrentSession((prev)=>{
-    const updatedCurrentSession = {...prev, ...sentState}
-    return {...updatedCurrentSession}
-    });
+    stateDispatch({ type: 'SAVE', sentState })
   };
 
-    return (
-      <div className="App">
-          <NavBar />
-         <Route render={({location})=>(
-          <TransitionGroup>
-            <CSSTransition key={location.key} timeout={450} classNames="fade">
+  return (
+    <div className="App">
+      <NavBar />
+      <Route render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition key={location.key} timeout={450} classNames="fade">
             <Switch location={location}>
-            <Route path="/task-creator" >
-              <TaskCreator onTaskCreation={handleTaskCreation}/>
-            </Route>
-            <Route path="/task-manager" >
-              <TaskManager
-                onTaskPick={handleTaskPick}
-                onTaskDelete = {handleTaskDelete}
-                tasks={tasks}
-              />
-            </Route>
-            <Route path="/history">
-              <History tasks={tasks}
-              />
-            </Route>
-            <Route exact path="/">
-              <CurrentSession
-                saveState={handleSaveState}
-                currentSession={currentSession}
-              />
-            </Route>
-          </Switch>
+              <Route path="/task-creator" >
+                <TaskCreator
+                  onTaskCreation={handleTaskCreation}
+                />
+              </Route>
+              <Route path="/task-manager" >
+                <TaskManager
+                  onTaskPick={handleTaskPick}
+                  onTaskDelete={handleTaskDelete}
+                  tasks={state.tasks}
+                />
+              </Route>
+              <Route path="/history">
+                <History tasks={state.tasks}
+                />
+              </Route>
+              <Route exact path="/">
+                <CurrentSession
+                  saveState={handleSaveState}
+                  currentSession={state.currentSession}
+                />
+              </Route>
+            </Switch>
           </CSSTransition>
-          </TransitionGroup>
-         )}/>
-            
-          
-      </div>
-    );
+        </TransitionGroup>
+      )} />
+
+
+    </div>
+  );
 }
 
 export default App;
